b0dd791feffdc52f99d21055f72149dab13b627d
gnzlplcs <palaciosgm@outlook.com>
2023-02-24 08:21:24 -0500

    :construction: setup & BrowserRouter complete

--- intro/src/main.jsx
+++ intro/src/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <h1 >Hello world</h1>
-  </React.StrictMode>,
+  <Router>
+    <App />
+  </Router>,
 )
