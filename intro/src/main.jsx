f9e7d6cee90d2193f2075b5f75bcf681b3ebb2a2
gnzlplcs <palaciosgm@outlook.com>
2023-02-25 18:42:56 -0500

    :construction: route, path & element

--- intro/src/main.jsx
+++ intro/src/main.jsx
@@ -2,9 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
+import About from "./About";
+
+/**
+ * Challenge:
+ * 1. Create an "About" component (just render another h1 that says
+ *    "About page" or something more interesting of your choosing).
+ * 2. Create a new Route to render the About component when the path
+ *    is /about
+ */
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
-    <Routes></Routes>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   </Router>
 );
