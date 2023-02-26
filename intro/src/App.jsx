ff2538402b4f949678bae5f4da6e39dfc3994457
gnzlplcs <palaciosgm@outlook.com>
2023-02-25 19:23:39 -0500

    :construction: Link

--- intro/src/App.jsx
+++ intro/src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 
 const App = () => {
   return (
     <Router>
+      <nav>
+        <Link to="/">Home</Link>&nbsp;
+        <Link to="/about">About</Link>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
