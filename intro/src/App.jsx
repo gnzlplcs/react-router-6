98c306ca707f4369b469cb1f622e6b898dbf7e9d
gnzlplcs <palaciosgm@outlook.com>
2023-02-25 19:01:00 -0500

    :construction: quick re-org

--- intro/src/App.jsx
+++ intro/src/App.jsx
@@ -1,5 +1,17 @@
-function App() {
-  return <h1>Hello React Router!</h1>;
-}
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
