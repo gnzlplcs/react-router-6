75e37536871d825f4434b22432011a883a58ce04
gnzlplcs <palaciosgm@outlook.com>
2023-02-27 09:08:24 -0500

    :boom: vanlight challenge completed
    my solution

--- 02-vanlife-project/src/About.jsx
+++ 02-vanlife-project/src/About.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import heroImg from "./assets/image54.png";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <main className="about main">
+      <div className="about__hero-img">
+        <img src={heroImg} alt="Van camping in the night" />
+      </div>
+      <div className="about__content">
+        <h1 className="about__title">
+          Don’t squeeze in a sedan when you could relax in a van.
+        </h1>
+        <p>
+          Our mission is to enliven your road trip with the perfect travel van
+          rental. Our vans are recertified before each trip to ensure your
+          travel plans can go off without a hitch.
+          <br />
+          (Hitch costs extra 😉)
+        </p>
+        <p>
+          Our team is full of vanlife enthusiasts who know firsthand the magic
+          of touring the world on 4 wheels.
+        </p>
+        <div className="about__highlight">
+          <h3>Your destination is waiting.</h3>
+          <h3>Your van is ready.</h3>
+          <button className="about__highlight-button">Explore our vans</button>
+        </div>
+      </div>
+    </main>
+  );
+};
 
-export default About
+export default About;
--- 02-vanlife-project/src/Home.jsx
+++ 02-vanlife-project/src/Home.jsx
@@ -2,16 +2,18 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div class="home">
-      <h1>You got the travel plans, we got the travel vans.</h1>
-      <div>
+    <main class="home main">
+      <div className="home__title">
+        <h1>You got the travel plans, we got the travel vans.</h1>
+      </div>
+      <div className="home__content">
         <p>
           Add adventure to your life by joining the #vanlife movement. Rent the
           perfect van to make your perfect road trip.
         </p>
-        <button>Find your van</button>
+        <button className="home__button">Find your van</button>
       </div>
-    </div>
+    </main>
   );
 };
 
